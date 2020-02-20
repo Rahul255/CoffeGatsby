1ee789a51715f504617e2b8152dfe0b78acf1a03
Rahul255 <rahulmohan255@gmail.com>
2020-02-20 12:02:30 +0530

    file added

---
+++ src/components/Home/info.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'gatsby'
+
+export default function Info(){
+    return(
+       <section className="py-5">
+           <div className="container">
+               <div className="row">
+                   <div className="col-10 col-sm-8 mx-auto text-center">
+                       <p className="lead text-muted mb-5">
+                        InCTF was started to generate awareness and interest in cybersecurity in the student community
+
+                            More importantly, equipping students with the skills to pursue careers in this sector, and provide hands-on training and exposure to industry experts
+
+                            Team bi0s was the first Indian team to compete in international cybersecurity contests, back in 2007
+
+                            We look to bring the same passion and interest to the whole country, allowing them to take on the challenges of the rapidly changing digital world.
+                       </p>
+                   </div>
+               </div>
+           </div>
+       </section>
+    )
+}
