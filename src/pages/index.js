b5526a1019dec9fd732c2ada6e45163369fd02ef
Rahul255 <rahulmohan255@gmail.com>
2020-02-26 11:48:45 +0530

    file added

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,8 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import BackgroundSection from '../components/Globals/BackgroundSection';
 import Info from '../components/Home/info';
+import menu from '../components/Home/Menu';
+import Menu from "../components/Home/Menu";
 
 const IndexPage = ({data}) => (
   <Layout>
@@ -16,7 +18,7 @@ const IndexPage = ({data}) => (
     styleClass="default-background"
     />
     <Info />
-   
+    <Menu items={data.menu}/>
   </Layout>
 );
 
@@ -27,6 +29,21 @@ export const query = graphql`
     childImageSharp {
       fluid{
         ...GatsbyImageSharpFluid_tracedSVG
+      }
+    }
+  }
+  menu:allContentfulCoffeItem{
+    edges{
+      node {
+        id
+        title
+        category
+        image{
+          fixed(width:80,height:80){
+          ...GatsbyContentfulFixed_tracedSVG
+        }
+         
+        }
       }
     }
   }
